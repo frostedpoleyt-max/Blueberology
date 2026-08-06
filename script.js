cfe5bd2febe347f372cdb75c6be073064ee383bf
frostedpoleyt-max <frostedpoleyt@gmail.com>
2026-08-06 18:54:23 +1200

    Refactor click event listener for favorite button

--- script.js
+++ script.js
@@ -1148,15 +1148,12 @@ document.addEventListener(
 "click",
 function(event){
 
-    if(
-    event.target.classList.contains("favorite-btn")
-    ){
+if(
+event.target.classList.contains("favorite-btn")
+){
 
-        renderDictionary();
+renderDictionary();
 
-    }
+}
 
 });
-
-
-alert("Blueberology JS loaded");
