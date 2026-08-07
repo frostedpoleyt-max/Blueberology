5b852577236476c0aeb10dbb6e43952e90305ce9
frostedpoleyt-max <frostedpoleyt@gmail.com>
2026-08-07 18:17:20 +1200

    Improve shuffle function implementation
    Refactor shuffle function to use Fisher-Yates algorithm for better randomness.

--- script.js
+++ script.js
@@ -858,9 +858,16 @@ let answering = false;
 
 function shuffle(array){
 
-return array.sort(
-()=>Math.random()-0.5
-);
+for(let i=array.length-1;i>0;i--){
+
+const j=Math.floor(Math.random()*(i+1));
+
+[array[i],array[j]] =
+[array[j],array[i]];
+
+}
+
+return array;
 
 }
 
